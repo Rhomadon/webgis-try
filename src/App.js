edc4881f35c0455b3fc6a6b8229c78301ebf44a4
Rhomadon <adonrhomadon100@gmail.com>
2022-09-20 10:53:05 +0700

    Webgis Connected Api Geojson

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
 import Draggable from './component/Draggable';
 
 function App() {
   const center = [-6.23363948370361, 106.8215857154487]
   return (
     <div>
-      <MapContainer center={center} zoom={11} scrollWheelZoom={true}>
+      <MapContainer center={center} zoom={11} scrollWheelZoom={true} maxZoom={18} minZoom={2}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Draggable />
+        <LayersControl position="topright">
+          <Draggable />
+        </LayersControl>
       </MapContainer>
     </div>
   );
--- src/component/Draggable.jsx
+++ src/component/Draggable.jsx
@@ -1,99 +1,73 @@
-import React from 'react'
-import { useCallback, useState, useRef, useMemo } from 'react'
-import { Marker, Popup, GeoJSON } from 'react-leaflet'
+import React, { useCallback, useState, useRef, useMemo, useEffect } from 'react'
+import axios from 'axios'
+import { GeoJSON, LayersControl } from 'react-leaflet'
 
 export default function Draggable() {
-	const center = [-6.23363948370361, 106.8215857154487]
+	const [draggable, setDraggable] = useState(false)
+  const [features, setFeatures] = useState([])
 
-  // const dataGeo = {
-  // "type": "FeatureCollection",
-  // "features": [
-  //   {
-  //     "type": "Feature",
-  //     "geometry": {
-  //       "type": "Point",
-  //       "coordinates": [
-  //         106.79375,
-  //         -6.1625
-  //       ]
-  //     },
-  //     "properties": {
-  //       "objectid": 1,
-  //       "liquidity": 0.0453797593904,
-  //       "meshcode3": "6009061693",
-  //       "type": "Point",
-  //       "longitude": "106.79375000026249",
-  //       "latitude": "-6.162500000174703"
-  //     }
-  //   },
-  //   {
-  //     "type": "Feature",
-  //     "geometry": {
-  //       "type": "Point",
-  //       "coordinates": [
-  //         106.83125,
-  //         -6.329166665
-  //       ]
-  //     },
-  //     "properties": {
-  //       "objectid": 2,
-  //       "liquidity": 0.0327870562585,
-  //       "meshcode3": "6009063696",
-  //       "type": "Point",
-  //       "longitude": "106.83125000018754",
-  //       "latitude": "-6.3291666650094385"
-  //     }
-  //   },
-  //   {
-  //     "type": "Feature",
-  //     "geometry": {
-  //       "type": "Point",
-  //       "coordinates": [
-  //         106.84375,
-  //         -6.229166665
-  //       ]
-  //     },
-  //     "properties": {
-  //       "objectid": 3,
-  //       "liquidity": 0.0352258957937,
-  //       "meshcode3": "6009062677",
-  //       "type": "Point",
-  //       "longitude": "106.84375000016257",
-  //       "latitude": "-6.229166665209277"
-  //     }
-  //   }]}
+	const axiosData = () => {
+		const url = 'http://localhost:4000/liquidity-rent/api'
+		axios.get(url).then(res => {
+			setFeatures(res.data)
+		}).catch(err => {
+			console.log(err.message)
+		})
+	}
 
-  const [draggable, setDraggable] = useState(false)
-  const [position, setPosition] = useState(center)
-  const markerRef = useRef(null)
-	const eventHandlers = useMemo(
-		() => ({
-			dragend() {
-				const marker = markerRef.current
-				if (marker != null) {
-					setPosition(marker.getLatLng())
-				}
-			}
-		}), [])
+	const geoJsonLayerRef = useRef(null)
+	const isMountRef = useRef(true)
 
-  const toggleDraggable = useCallback(() => {
+	useEffect(() => {
+		if (isMountRef.current) {
+			axiosData()
+			isMountRef.current = false
+		}
+	}, [])
+
+	useEffect(() => {
+		const layer = geoJsonLayerRef.current
+		if (layer) {
+			console.log("Api Liquidity-Rent")
+			layer.clearLayers().addData(features)
+		}
+	}, [features])
+
+	// const eventHandlers = useMemo(
+	// 	() => ({
+	// 		dragend() {
+	// 			const marker = geoJsonLayerRef.current
+	// 			console.log(marker)
+				// if (marker != null) {
+				// 	setFeatures(marker.getLatLng())
+				// }
+		// 	}
+		// }), [])
+
+	const toggleDraggable = useCallback(() => {
     setDraggable((d) => !d)
-  }, [])
+	}, [])
+
+	const onEachFeature = (feature, layer) => {
+		let popupContent =
+			"<pre>" +
+			JSON.stringify(feature.properties, null, " ").replace(/[\{\}"]/g, "") +
+			"</pre>"
+
+		layer.bindPopup(popupContent)
+		layer.on('dragend', function(e){
+        document.getElementById("latitude").value = layer.getLatLng().latitude;
+        document.getElementById("longitude").value = layer.getLatLng().longitude;
+    })
+	}
 
 	return (
-		<Marker
-			draggable={draggable}
-			eventHandlers={eventHandlers}
-      // data={position}
-      position={position}
-			ref={markerRef} >
-			<Popup minWidth={90}>
-				<span onClick={toggleDraggable}>
-					{draggable
-						? 'Marker is draggable'
-						: 'Click here to make marker draggable'}
-				</span>
-			</Popup>
-		</Marker>
+		<LayersControl.Overlay name="Liquidity Rent">
+			<GeoJSON
+				data={features}
+				onEachFeature={onEachFeature}
+				ref={geoJsonLayerRef}
+			/>
+		</LayersControl.Overlay>
   )
 }
