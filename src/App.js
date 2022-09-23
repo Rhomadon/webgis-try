644878920438ed5cdbe1e0fdaeae6565f8de30af
Rhomadon <adonrhomadon100@gmail.com>
2022-09-23 10:26:35 +0700

    Draggable Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
 import Draggable from './component/Draggable';
-import { DraggablePathDrag } from './component/DraggablePathDrag';
 
 function App() {
   const center = [-6.23363948370361, 106.8215857154487]
-  // const center = [39.63563779557324, -104.99234676361085]
   return (
     <div>
       <MapContainer center={center} zoom={11} scrollWheelZoom={true} maxZoom={18} minZoom={2}>
@@ -15,7 +13,6 @@ function App() {
         />
         <LayersControl position="topright">
           <Draggable />
-          {/* <DraggablePathDrag /> */}
         </LayersControl>
       </MapContainer>
     </div>
--- src/component/Draggable.jsx
+++ src/component/Draggable.jsx
@@ -1,12 +1,12 @@
-import React, { useCallback, useState, useRef, useMemo, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import axios from 'axios'
-import { GeoJSON, LayersControl, Popup } from 'react-leaflet'
+import { GeoJSON, LayersControl } from 'react-leaflet'
 import * as L from 'leaflet'
+// import markerClusterGroup from 'react-leaflet-cluster'
+import 'leaflet-path-drag';
 
 export default function Draggable() {
-	const [draggable, setDraggable] = useState(false)
-	const [features, setFeatures] = useState([])
-	const [coordinates, setCoordinates] = useState([])
+  const [features, setFeatures] = useState([])
 
 	const axiosData = () => {
 		const url = 'http://localhost:4000/liquidity-rent/api'
@@ -15,18 +15,6 @@ export default function Draggable() {
 		}).catch(err => {
 			console.log(err.message)
 		})
-		axios.get(url).then(res => {
-			setCoordinates(res.data.features)
-		}).catch(err => {
-			console.log(err.message)
-		})
-	}
-
-	const feature = (e) => {
-		const latLngs = e.target.getLatLng().lat
-		console.log(latLngs)
-		const coordinates = latLngs.map((point) => [point.lng, point.lat])
-		console.log(coordinates)
 	}
 
 	const geoJsonLayerRef = useRef(null)
@@ -40,27 +28,12 @@ export default function Draggable() {
 	}, [])
 
 	useEffect(() => {
-		const layer = geoJsonLayerRef.current
-		if (layer) {
-			console.log("Api Liquidity-Rent")
-			layer.clearLayers().addData(features)
-		}
-	}, [features])
-
-	const eventHandlers = useMemo(
-		() => ({
-			dragend() {
-				const marker = geoJsonLayerRef.current
-				console.log(marker)
-				if (marker != null) {
-					setFeatures(marker.getLatLng())
-				}
+			const layer = geoJsonLayerRef.current
+			if (layer) {
+				console.log("Api Liquidity-Rent")
+				layer.clearLayers().addData(features)
 			}
-		}), [])
-
-	const toggleDraggable = useCallback(() => {
-    setDraggable((d) => !d)
-	}, [])
+	}, [features])
 
 	const onEachFeature = (feature, layer) => {
 		let popupContent =
@@ -68,39 +41,41 @@ export default function Draggable() {
 			JSON.stringify(feature.properties, null, " ").replace(/[\{\}"]/g, "") +
 			"</pre>"
 
-		console.log(feature.properties)
-
 		layer.bindPopup(popupContent)
-		layer.on('dragend', function(e){
-        document.getElementById("latitude").value = layer.getLatLng().lat;
-        document.getElementById("longitude").value = layer.getLatLng().lng;
-		})
 	}
+
+	// const markers = L.markerClusterGroup()
 
 	const pointToLayer = (feature, latlng) => {
 		return L.circleMarker(latlng, { draggable: true }, {
-			radius: 8,
-			fillColor: "#ff7800",
-			color: "#000",
-			weight: 1,
-			opacity: 1,
-			fillOpacity: 0.8
-		})
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 0.5,
+        fillOpacity: 0.5
+			})
 	}
+
+	// const markerCluster = (feature, latlng) => {
+	// 	return markers.addLayer(L.circleMarker(latlng))
+	// }
 
 	return (
 		<LayersControl.Overlay name="Liquidity Rent">
 			<GeoJSON
 				data={features}
-				// onEachFeature={onEachFeature}
+				markerCluster
+				pointToLayer={pointToLayer}
+				onEachFeature={onEachFeature}
 				ref={geoJsonLayerRef}
-				eventHandlers={eventHandlers}
-			// pointToLayer={pointToLayer}
-			>
-				<Popup minWidth={90}>
-					<button>Text</button>
-				</Popup>
-			</GeoJSON>
+				style={() => ({
+            color: '#4a83ec',
+            weight: 0.5,
+            fillColor: "#1a1d",
+            fillOpacity: 1,
+          })}
+				/>
 		</LayersControl.Overlay>
   )
 }
